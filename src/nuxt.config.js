e937d541f71596d4752d1219262c81febfd5d724
Adrian Valero <adrian.valero@nexum.com>
2021-06-25 16:43:20 +0200

    Add SCSS support

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -20,6 +20,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    // SCSS file in the project
+    '@/assets/css/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -41,5 +43,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  env: {
+
   }
 }

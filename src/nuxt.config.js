e4534a338932e2c7426bd018147f0ca843f5d9ed
Adrian Valero <adrian.valero@nexum.com>
2021-06-29 09:29:38 +0200

    Webpack dependency

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -56,6 +56,11 @@ export default {
     port: process.env.PORT || 3000 // default: 3000
   },
 
+  axios: {
+    baseURL: `http://localhost:${process.env.PORT || 3000}/`,
+    browserBaseURL: "/"
+  },
+
   router: {
     base: '/index/',
     routeNameSplitter: '/'
